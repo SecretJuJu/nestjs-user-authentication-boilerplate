4da4e8aad563dc73590bc242a5686449351dccb2
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 18:11:13 +0900

    fix: conflictException 에러 핸들링

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../user/dtos/create-user.dto';
 import { UserService } from '../user/user.service';
 
@@ -7,6 +7,13 @@ export class AuthService {
   constructor(private usersService: UserService) {}
 
   async registerUser(createUserDto: CreateUserDto): Promise<any> {
+    const { username } = createUserDto;
+    const findedUser = await this.usersService.findUser(username);
+
+    if (findedUser !== null) {
+      throw new ConflictException();
+    }
+
     const user = await this.usersService.createUser(createUserDto);
     return user.username;
   }
