3d1d0a61c9b3cc6ae3fdee433415c132324e9859
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 16:17:59 +0900

    feat: userService createUser 기능 추가
    - 패스워드 암호화를 위한 bcrypt 모듈 설치

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { genSaltSync, hashSync } from 'bcrypt';
 import { Model } from 'mongoose';
+
 import { User, UserDocument } from 'src/schemas/user.schema';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const password = createUserDto.password;
+    const saltRounds = 10;
+    const salt = genSaltSync(saltRounds);
+    const hashedPassword = hashSync(password, salt);
+
+    createUserDto.password = hashedPassword;
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
 }
