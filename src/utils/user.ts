fd845d7a74385d6699c8d69421b1aa276ca48875
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 18:18:19 +0900

    add: utils/user 에 checkPassword 함수 추가

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,4 +1,4 @@
-import { genSaltSync, hashSync } from 'bcrypt';
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
 
 export const createHashedPassword = (password: string) => {
   const saltRounds = 10;
@@ -6,4 +6,9 @@ export const createHashedPassword = (password: string) => {
   const hashedPassword = hashSync(password, salt);
 
   return hashedPassword;
+};
+
+export const checkPassword = (password: string, hashedPassword: string) => {
+  const isPasswordCorrect = compareSync(password, hashedPassword); // hash.toString for type checking hack
+  return isPasswordCorrect;
 };
