7f19e63c109adbcfccc2c86e65e1a6b1557bf7e5
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 17:11:33 +0900

    refactor: utils 를 app 밖으로 이동

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { User, UserDocument } from 'src/schemas/user.schema';
-import { createHashedPassword } from '../utils/user';
+import { createHashedPassword } from 'src/utils/user';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
