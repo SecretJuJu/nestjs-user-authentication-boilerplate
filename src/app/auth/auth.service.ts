e943d9fae84a3af9e12372a4021f69091073285a
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 17:22:55 +0900

    feat: register user 기능 추가

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../user/dtos/create-user.dto';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UserService) {}
+
+  async registerUser(createUserDto: CreateUserDto): Promise<any> {
+    const user = await this.usersService.createUser(createUserDto);
+    return user.username;
+  }
+}
