d7c78fc90fb0c7fea9c5bb1a0255736b6bd3a50b
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 18:11:37 +0900

    feat: register 기능 추가

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from '../user/dtos/create-user.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @Post('/register')
+  @UsePipes(ValidationPipe)
+  async register(@Body() createUserDto: CreateUserDto): Promise<string> {
+    const createdUsername = await this.authService.registerUser(createUserDto);
+    return createdUsername;
+  }
 }
