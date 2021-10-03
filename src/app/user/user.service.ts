a64b70d7407f0a73e84f7ae61ff38e6b1d709f16
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-03 16:01:29 +0900

    add: UserModel 을 user.service 안에 DI

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+}
