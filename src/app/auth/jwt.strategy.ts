8bda20d5bbb52cfac7526c49c3994df572680e05
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-04 18:21:51 +0900

    add: jwt.strategy 추가

---
+++ src/app/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+import { jwtConstants } from './constants';
+import { User } from 'src/schemas/user.schema';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: User) {
+    return { username: payload.username };
+  }
+}
